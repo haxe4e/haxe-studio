5d14bdfb55e8a662b0c04ba5c5314e46e7f3b36e
sebthom <sebthom@users.noreply.github.com>
2021-11-04 18:17:12 +0100

    configure workbench defaults

---
+++ plugin/src/main/java/org/haxe4e/studio/HaxeStudioApplication.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2021 by the Haxe4E authors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.haxe4e.studio;
+
+import java.net.URL;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
+import org.eclipse.equinox.app.IApplicationContext;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.Workbench;
+import org.eclipse.ui.internal.WorkbenchPlugin;
+import org.eclipse.ui.internal.ide.ChooseWorkspaceDialog;
+import org.eclipse.ui.internal.ide.IDEInternalPreferences;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.ide.application.DelayedEventsProcessor;
+import org.eclipse.ui.internal.ide.application.IDEApplication;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+/**
+ * @author Sebastian Thomschke
+ */
+@SuppressWarnings("restriction")
+public class HaxeStudioApplication extends IDEApplication {
+
+   /**
+    * Only overriding {@link IDEApplication#start(IApplicationContext)} to configure custom WorkbenchAdvisor
+    */
+   @Override
+   public Object start(final IApplicationContext ctx) throws Exception {
+      Job.getJobManager().suspend();
+
+      final var display = createDisplay();
+
+      try {
+         final var processor = new DelayedEventsProcessor(display);
+
+         final var shell = WorkbenchPlugin.getSplashShell(display);
+         if (shell != null) {
+            shell.setText(ChooseWorkspaceDialog.getWindowTitle());
+            shell.setImages(Window.getDefaultImages());
+         }
+
+         final var instanceLocationCheck = checkInstanceLocation(shell, ctx.getArguments());
+         if (instanceLocationCheck != null) {
+            WorkbenchPlugin.unsetSplashShell(display);
+            return instanceLocationCheck;
+         }
+
+         final var rc = PlatformUI.createAndRunWorkbench(display, new HaxeStudioWorkbenchAdvisor(processor));
+
+         if (rc != PlatformUI.RETURN_RESTART)
+            return EXIT_OK;
+
+         return EXIT_RELAUNCH.equals(Integer.getInteger(Workbench.PROP_EXIT_CODE)) ? EXIT_RELAUNCH : EXIT_RESTART;
+
+      } finally {
+         display.dispose();
+
+         final var instanceLoc = Platform.getInstanceLocation();
+         if (instanceLoc != null) {
+            instanceLoc.release();
+         }
+      }
+   }
+
+   @Override
+   protected ReturnCode checkValidWorkspace(final Shell shell, final URL url) {
+
+      /* prevent "Workspace was written with an older version of the product" warning.
+       * see https://stackoverflow.com/questions/63029131/is-there-a-way-to-force-an-update-to-the-eclipse-workspace
+       */
+      final var workbenchPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, IDEWorkbenchPlugin.IDE_WORKBENCH);
+      workbenchPrefs.setValue(IDEInternalPreferences.WARN_ABOUT_WORKSPACE_INCOMPATIBILITY, false);
+
+      return super.checkValidWorkspace(shell, url);
+   }
+}
---
+++ plugin/src/main/java/org/haxe4e/studio/HaxeStudioWorkbenchAdvisor.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2021 by the Haxe4E authors.
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.haxe4e.studio;
+
+import org.eclipse.core.runtime.preferences.ConfigurationScope;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
+import org.eclipse.ui.internal.ide.application.DelayedEventsProcessor;
+import org.eclipse.ui.internal.ide.application.IDEWorkbenchAdvisor;
+import org.eclipse.ui.preferences.ScopedPreferenceStore;
+
+/**
+ * @author Sebastian Thomschke
+ */
+@SuppressWarnings("restriction")
+public class HaxeStudioWorkbenchAdvisor extends IDEWorkbenchAdvisor {
+
+   public HaxeStudioWorkbenchAdvisor(final DelayedEventsProcessor processor) {
+      super(processor);
+   }
+
+   /**
+    * Executed before {@link #preStartup()}
+    */
+   @Override
+   public void initialize(final IWorkbenchConfigurer configurer) {
+      super.initialize(configurer);
+
+      configureWorkspaceDefaults();
+   }
+
+   private void configureWorkspaceDefaults() {
+
+      final var resPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.core.resources");
+      resPrefs.setDefault("encoding", "UTF-8");
+      resPrefs.setDefault("refresh.lightweight.enabled", true);
+
+      final var uiPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.ui");
+      uiPrefs.setDefault("ENABLE_ANIMATIONS", false);
+      uiPrefs.setDefault("SHOW_MEMORY_MONITOR", true);
+      uiPrefs.setDefault("SHOW_TEXT_ON_PERSPECTIVE_BAR", false);
+
+      final var uiConsolePrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.ui.console");
+      uiConsolePrefs.setDefault(".P_CONSOLE_WORD_WRAP", false); // for https://github.com/mihnita/ansi-econsole
+
+      final var uiDebugPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.debug.ui");
+      uiDebugPrefs.setDefault("Console.limitConsoleOutput", false); // for https://github.com/mihnita/ansi-econsole
+
+      final var uiEditorsPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.ui.editors");
+      uiEditorsPrefs.setDefault("lineNumberRuler", true);
+      uiEditorsPrefs.setDefault("printMargin", true);
+      uiEditorsPrefs.setDefault("printMarginAllowOverride", true);
+      uiEditorsPrefs.setDefault("printMarginColumn", 120);
+      uiEditorsPrefs.setDefault("spacesForTabs", false);
+
+      uiEditorsPrefs.setDefault("showWhitespaceCharacters", true);
+      uiEditorsPrefs.setDefault("showLeadingIdeographicSpaces", false);
+      uiEditorsPrefs.setDefault("showLeadingSpaces", false);
+      uiEditorsPrefs.setDefault("showLeadingTabs", false);
+      uiEditorsPrefs.setDefault("showEnclosedIdeographicSpaces", false);
+      uiEditorsPrefs.setDefault("showEnclosedSpaces=", false);
+      uiEditorsPrefs.setDefault("showEnclosedTabs", false);
+      uiEditorsPrefs.setDefault("showTrailingIdeographicSpaces", true);
+      uiEditorsPrefs.setDefault("showTrailingSpaces", true);
+      uiEditorsPrefs.setDefault("showTrailingTabs", true);
+      uiEditorsPrefs.setDefault("showCarriageReturn", false);
+      uiEditorsPrefs.setDefault("showLineFeed", false);
+      uiEditorsPrefs.setDefault("whitespaceCharacterAlphaValue", 40);
+
+      final var uiWbPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.ui.workbench");
+      uiWbPrefs.setDefault("showCommandKeysForMouseEvents", true); // https://www.eclipse.org/eclipse/news/4.13/platform.php#show-keybinding
+
+      final var cmpPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.compare");
+      cmpPrefs.setDefault("org.eclipse.compare.IgnoreWhitespace", true);
+      cmpPrefs.setDefault("org.eclipse.compare.UseOutlineView", true);
+      cmpPrefs.setDefault("org.eclipse.compare.OpenStructureCompare", false); // to prevent https://bugs.eclipse.org/bugs/show_bug.cgi?id=509820
+
+      final var teamPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.team.ui");
+      teamPrefs.setDefault("org.eclipse.team.ui.compress_folders", true);
+      teamPrefs.setDefault("org.eclipse.team.ui.default_layout", "org.eclipse.team.ui.compressed_layout");
+      teamPrefs.setDefault("org.eclipse.team.ui.run_import_in_background_", true);
+
+      final var egitCorePrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.egit.core");
+      egitCorePrefs.setDefault("core_defaultRepositoryDir", "${workspace_loc}");
+
+      final var egitUiPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "org.eclipse.egit.ui");
+      egitUiPrefs.setDefault("decorator_filetext_decoration", "{name}");
+      egitUiPrefs.setDefault("decorator_foldertext_decoration", "{name}");
+      egitUiPrefs.setDefault("decorator_projecttext_decoration", "{name} [{repository }{branch}{ branch_status}]");
+      egitUiPrefs.setDefault("decorator_submoduletext_decoration", "{name} [{branch}{ branch_status}]{ short_message}");
+      egitUiPrefs.setDefault("decorator_show_dirty_icon", true);
+      egitUiPrefs.setDefault("show_push_success_dialog", false);
+
+      final var intentGuidePrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "net.certiv.tools.indentguide");
+      intentGuidePrefs.setDefault("indentguide.enabled", true);
+      intentGuidePrefs.setDefault("indentguide.line_style", 3);
+
+      final var ttdPrefs = new ScopedPreferenceStore(ConfigurationScope.INSTANCE, "net.sf.ttd.core");
+      ttdPrefs.setDefault("net.sf.ttd.core.config.impl.SerializerVersion1.count", 2);
+      ttdPrefs.setDefault("net.sf.ttd.core.config.impl.SerializerVersion1.image0TOP_LEFT", "Exclamation Blue");
+      ttdPrefs.setDefault("net.sf.ttd.core.config.impl.SerializerVersion1.image1TOP_LEFT", "Exclamation");
+      ttdPrefs.setDefault("net.sf.ttd.core.config.impl.SerializerVersion1.taskTagName0", "todo");
+      ttdPrefs.setDefault("net.sf.ttd.core.config.impl.SerializerVersion1.taskTagName1", "fixme");
+   }
+}
