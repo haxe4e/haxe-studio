d7b047267b10d4931e69148250a71f0275497fb2
sebthom <sebthom@users.noreply.github.com>
2024-08-19 22:47:23 +0200

    fix: java binary executable flag in Linux/MacOS tar.gz files missing

--- product/fix_exec_flag_in_archives.py
+++ product/fix_exec_flag_in_archives.py
@@ -6,26 +6,38 @@
 # Sets the missing executable flag in tar.gz files produced for Linux/MacOS when building on Windows
 # https://bugs.eclipse.org/bugs/show_bug.cgi?id=442607
 
-import os, tarfile
+import fnmatch, os, tarfile
 
 
 def set_executable(archivepath:str, files_to_change:list[str]):
     print(f"Fixing file permission in {archivepath}...")
+
     os.rename(archivepath, f"{archivepath}.orig")
+
     source:tarfile.TarFile
     target:tarfile.TarFile
+
     with tarfile.open(f"{archivepath}.orig", "r:gz") as source:
         with tarfile.open(archivepath, "w:gz", compresslevel = 9) as target:
             entry: tarfile.TarInfo
             for entry in source.getmembers():
-                if entry.path in files_to_change:
+                if any(fnmatch.fnmatch(entry.path, pattern) for pattern in files_to_change):
                     entry.mode = 0o755
-                target.addfile(entry, source.extractfile(entry))
+
+                if entry.isfile():
+                    target.addfile(entry, source.extractfile(entry))
+                else:
+                    target.addfile(entry)
 
     os.remove(f"{archivepath}.orig")
 
 
 cwd = os.path.dirname(__file__)
 
-set_executable(f"{cwd}/target/products/org.haxe4e.studio-linux.gtk.x86_64.tar.gz", ["HaxeStudio"])
-set_executable(f"{cwd}/target/products/org.haxe4e.studio-macosx.cocoa.x86_64.tar.gz", ["Eclipse.app/Contents/MacOS/HaxeStudio"])
+set_executable(f"{cwd}/target/products/org.haxe4e.studio-linux.gtk.x86_64.tar.gz", [
+    "HaxeStudio",
+    "plugins/org.eclipse.justj.openjdk.hotspot.jre.*/jre/bin/java"])
+
+set_executable(f"{cwd}/target/products/org.haxe4e.studio-macosx.cocoa.x86_64.tar.gz", [
+    "Eclipse.app/Contents/MacOS/HaxeStudio",
+    "Eclipse.app/Contents/Eclipse/plugins/org.eclipse.justj.openjdk.hotspot.jre.*/jre/bin/java"])
